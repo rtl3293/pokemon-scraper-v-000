be2bbf8399f5204a07d5f0e381aea191f7d74168
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-14 01:31:25 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -24,7 +24,7 @@ class Pokemon
       new_pokemon[header] = value
       #binding.pry
     end
-    hp_update = db.prepare("UPDATE hp FROM pokemon WHERE id = ?")
+    hp_update = db.prepare("UPDATE hp FROM pokemon WHERE id = (?)")
     hp_update.execute!(id)
     new_pokemon[:db] = @db
     Pokemon.new(new_pokemon).hp =
