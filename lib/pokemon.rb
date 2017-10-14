17c41d9d7f9af7074ff925b9557673008e500ba8
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-14 01:06:40 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,7 +18,8 @@ class Pokemon
   def self.find(id, db)
     prep = db.prepare("SELECT * FROM pokemon WHERE id = ?")
     new_pokemon = {}
-    prep.execute!(id)
+    prep.execute!(id).each_with_index do |value, i|
+      new_pokemon[prep.execute!(id)[i - 1].to_sym] = value
     binding.pry
   end
 end
